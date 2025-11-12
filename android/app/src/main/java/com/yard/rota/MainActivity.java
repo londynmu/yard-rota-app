57cf9a948b7fb70b52e505b7da99d577e3f03292
londynmu <Tideend@gmail.com>
2025-11-12 20:56:12 +0000

    android dalej pasek

--- android/app/src/main/java/com/yard/rota/MainActivity.java
+++ android/app/src/main/java/com/yard/rota/MainActivity.java
@@ -3,6 +3,11 @@ package com.yard.rota;
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 import androidx.core.view.WindowCompat;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -10,7 +15,21 @@ public class MainActivity extends BridgeActivity {
     // Switch from launch (splash) theme to main content theme
     setTheme(R.style.AppTheme_NoActionBar);
     super.onCreate(savedInstanceState);
+    // Force non-transparent status bar with dark icons and no content under it
+    final Window window = getWindow();
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.setStatusBarColor(Color.WHITE);
+    // Remove layout fullscreen flags and enable light status bar icons (API 23+)
+    final View decor = window.getDecorView();
+    int sysUi = decor.getSystemUiVisibility();
+    sysUi &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+    sysUi &= ~View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      sysUi |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+    }
+    decor.setSystemUiVisibility(sysUi);
     // Make system bars consume insets so WebView does not draw under status bar
-    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+    WindowCompat.setDecorFitsSystemWindows(window, true);
   }
 }
