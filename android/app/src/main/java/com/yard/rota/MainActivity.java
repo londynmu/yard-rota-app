6413120e5000fdce4bf7a6f95329792b4a4a2d51
londynmu <Tideend@gmail.com>
2025-11-12 20:23:54 +0000

    update do androida

--- android/app/src/main/java/com/yard/rota/MainActivity.java
+++ android/app/src/main/java/com/yard/rota/MainActivity.java
@@ -1,5 +1,16 @@
 package com.yard.rota;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Switch from launch (splash) theme to main content theme
+    setTheme(R.style.AppTheme_NoActionBar);
+    super.onCreate(savedInstanceState);
+    // Make system bars consume insets so WebView does not draw under status bar
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+  }
+}
